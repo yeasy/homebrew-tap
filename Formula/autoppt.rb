27529b4dde5787a6847b986c2464142649eee681
Baohua Yang <yangbaohua@gmail.com>
2026-05-17 21:20:46 -0700

    Update autoppt formula to 0.5.9 from GitHub source

--- Formula/autoppt.rb
+++ Formula/autoppt.rb
@@ -4,8 +4,9 @@
 class Autoppt < Formula
   desc "Generate Professional Presentations in Seconds using AI"
   homepage "https://github.com/yeasy/autoppt"
-  url "https://files.pythonhosted.org/packages/d9/7c/8e0692bf27f939375fe6312f35506d53e0347dd26a5b6f04b50e7594330e/autoppt-0.4.0.tar.gz"
-  sha256 "6d13d5938e628ac1e827d630cc8bae2c581f5633568331b2c34bfcf0275c4870"
+  url "https://github.com/yeasy/autoppt/archive/refs/tags/v0.5.9.tar.gz"
+  version "0.5.9"
+  sha256 "0084e9fdf545bfda8333cf531eecd7a96ffad3724c396beea3b61004c318e516"
   license "Apache-2.0"
 
   depends_on "pipx"
@@ -14,7 +15,7 @@ class Autoppt < Formula
   def install
     ENV["PIPX_HOME"] = libexec
     ENV["PIPX_BIN_DIR"] = libexec/"bin"
-    system "pipx", "install", "autoppt==#{version}", "--python", Formula["python@3.12"].opt_bin/"python3.12"
+    system "pipx", "install", buildpath.to_s, "--python", Formula["python@3.12"].opt_bin/"python3.12"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
